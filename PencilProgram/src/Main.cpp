95b7456d2880fecd2c4f04e3dc938925bbc865fd
Thomas-Tran <thomas.tran0789@gmail.com>
2023-05-17 15:35:49 -0500

    Moved simulated annealing process out of main() into a new function SimulateAnnealing(std::vector<Table>& tablesIn, std::vector<Dealer>& dealersIn, Push& pushIn) prep for threading

--- PencilProgram/src/Main.cpp
+++ PencilProgram/src/Main.cpp
@@ -5,7 +5,7 @@
 
 
 #if PEN_DEBUG
-const Log::LogLevel LOG_LEVEL = Log::Info;		// set log level here
+const Log::LogLevel LOG_LEVEL = Log::Warning;		// set log level here
 Log LOG;
 #endif
 
@@ -14,25 +14,25 @@ unsigned int Dealer::employeeNumberCounter;
 
 Push PopulateTables(std::vector<Table>& tables, std::vector<Dealer>& dealers);
 void CalculateFitness(Push& push, std::vector<Dealer>& dealers);
-bool findGameKnowledge(const Table::Games& gameName, Dealer* dealerPtr);
+bool FindGameKnowledge(const Table::Games& gameName, Dealer* dealerPtr);
+void SimulateAnnealing(std::vector<Table>& tablesIn, std::vector<Dealer>& dealersIn, Push& pushIn);
 
+static const int SUCCESSFUL_CHANGE_LIMIT = 1000;	// limit of successful changes per tempurature iteration
+static const int ATTEMPT_LIMIT = 15000;			// limit of attempts per tempurature iteration
+static const double e = 2.718281828;
+static double tempurature = 2000;					// starting temp, higher increases randomization
+static int changeCount;
+static int attemptCount;
+static int attemptNoChange;
+static double prob;			// random number between 0 and 1
+static double eCalc;			// ? Boltzman's distribution?
+static int bestFitness = 0;		// highest found fitness
 
 int main()
 {
 #if PEN_DEBUG
 	LOG.SetLogLevel(LOG_LEVEL);
 #endif
-
-	const int SUCCESSFUL_CHANGE_LIMIT = 1000;	// limit of successful changes per tempurature iteration
-	const int ATTEMPT_LIMIT = 15000;			// limit of attempts per tempurature iteration
-	const double e = 2.718281828;
-	double tempurature = 2000;					// starting temp, higher increases randomization
-	int changeCount;
-	int attemptCount;
-	int attemptNoChange;	
-	double prob;			// random number between 0 and 1
-	double eCalc;			// ? Boltzman's distribution?
-	int bestFitness = 0;		// highest found fitness
 
 	std::vector<Table> tables;
 	std::vector<Dealer> dealers;
@@ -43,66 +43,7 @@ int main()
 	Push first = PopulateTables(tables, dealers);
 	CalculateFitness(first, dealers);
 
-	do
-	{
-		tempurature = tempurature * .95;
-		attemptNoChange = 0;
-		changeCount = 0;
-		attemptCount = 0;
-
-		while (changeCount < SUCCESSFUL_CHANGE_LIMIT
-			&& attemptCount < ATTEMPT_LIMIT)
-		{			
-			int randTable = rand() % tables.size();		// select which table to change
-			int newDealer = rand() % dealers.size();	// select a random dealer
-			int oldFitness = first.fitness;				// fitness before change
-			Dealer* oldDealer = first.push[randTable].aDealerPtr;
-
-			if (first.fitness > bestFitness) bestFitness = first.fitness;
-			first.push[randTable].aDealerPtr = &dealers[newDealer];		// the change to be appraised
-
-			CalculateFitness(first, dealers);		// 'first' object has post-change fitness
-
-			prob = ((double)rand() / (RAND_MAX));		// random number between 0 and 1
-			eCalc = pow(e, ((first.fitness - oldFitness) / tempurature));
-
-			if (prob < eCalc && first.fitness != oldFitness)	// change accepted
-				changeCount++;
-			else
-			{
-				first.fitness = oldFitness;						// reset fitness
-				first.push[randTable].aDealerPtr = oldDealer;	// reset dealer
-				attemptNoChange++;
-			}
-			attemptCount++;
-		}
-	} while (attemptNoChange != ATTEMPT_LIMIT);
-
-#if PEN_DEBUG
-	LOG.LogError("final Fitness: " + std::to_string(first.fitness));
-	LOG.LogWarning("Highest Fitness: " + std::to_string(bestFitness));
-	
-	for (auto& p : first.push)
-	{
-		switch (p.aTable.gameName)
-		{
-		case Table::BJ:
-			LOG.LogWarning(std::to_string(p.aTable.number) + "     BJ     " + p.aDealerPtr->name);
-			break;
-		case Table::Rou:
-			LOG.LogWarning(std::to_string(p.aTable.number) + "     Rou    " + p.aDealerPtr->name);
-			break;
-		case Table::MB:
-			LOG.LogWarning(std::to_string(p.aTable.number) + "     MB     " + p.aDealerPtr->name);
-			break;
-		case Table::Poker:
-			LOG.LogWarning(std::to_string(p.aTable.number) + "     Poker  " + p.aDealerPtr->name);
-			break;
-		default:
-			LOG.LogError("Error: Invalid message in final Log");
-		}
-	}
-#endif
+	SimulateAnnealing(tables, dealers, first);
 
 	std::cout << "End of Program! Final Fitness: " << first.fitness << std::endl;
 	std::cin.get();
@@ -136,24 +77,24 @@ void CalculateFitness(Push& push, std::vector<Dealer>& dealers)
 		// Game Knowledge check
 		switch (a.aTable.gameName){
 		case Table::BJ:
-			if (findGameKnowledge(Table::BJ, a.aDealerPtr) 
+			if (FindGameKnowledge(Table::BJ, a.aDealerPtr) 
 				? push.fitness += 3 : push.fitness -= 25);
 			break;
 		case Table::Rou:
-			if (findGameKnowledge(Table::Rou, a.aDealerPtr)
+			if (FindGameKnowledge(Table::Rou, a.aDealerPtr)
 				? push.fitness += 3 : push.fitness -= 25);
 			break;
 		case Table::MB:
-			if (findGameKnowledge(Table::MB, a.aDealerPtr)
+			if (FindGameKnowledge(Table::MB, a.aDealerPtr)
 				? push.fitness += 5 : push.fitness -= 25);
 			break;
 		case Table::Poker:
-			if (findGameKnowledge(Table::Poker, a.aDealerPtr)
+			if (FindGameKnowledge(Table::Poker, a.aDealerPtr)
 				? push.fitness += 5 : push.fitness -= 25);
 			break;
 		default:
 #if PEN_DEBUG
-			LOG.LogError("Error: Invalid gameKnowledge fitness result!");
+			LOG.LogError("Error: Invalid gameKnowledge fitness result!");		// runs literally every iteration
 #endif
 			break;
 		}
@@ -170,11 +111,78 @@ void CalculateFitness(Push& push, std::vector<Dealer>& dealers)
 #endif
 }
 
-bool findGameKnowledge(const Table::Games& gameName, Dealer* dealerPtr)
+bool FindGameKnowledge(const Table::Games& gameName, Dealer* dealerPtr)
 {
 	for (Table::Games g : dealerPtr->gameKnowledge)
 	{
 		if (gameName == g) return true;
 	}
 	return false;
+}
+
+void SimulateAnnealing(std::vector<Table>& tablesIn, std::vector<Dealer>& dealersIn, Push& pushIn)
+{
+	do
+	{
+		tempurature = tempurature * .95;
+		attemptNoChange = 0;
+		changeCount = 0;
+		attemptCount = 0;
+
+		while (changeCount < SUCCESSFUL_CHANGE_LIMIT
+			&& attemptCount < ATTEMPT_LIMIT)
+		{
+			int randTable = rand() % tablesIn.size();		// select which table to change
+			int newDealer = rand() % dealersIn.size();	// select a random dealer
+			int oldFitness = pushIn.fitness;				// fitness before change
+			Dealer* oldDealer = pushIn.push[randTable].aDealerPtr;
+
+			if (pushIn.fitness > bestFitness) bestFitness = pushIn.fitness;
+			pushIn.push[randTable].aDealerPtr = &dealersIn[newDealer];		// the change to be appraised
+
+			CalculateFitness(pushIn, dealersIn);		// 'pushIn' object has post-change fitness
+
+			prob = ((double)rand() / (RAND_MAX));		// random number between 0 and 1
+			eCalc = pow(e, ((pushIn.fitness - oldFitness) / tempurature));
+
+			if (prob < eCalc && pushIn.fitness != oldFitness)	// change accepted
+				changeCount++;
+			else
+			{
+				pushIn.fitness = oldFitness;						// reset fitness
+				pushIn.push[randTable].aDealerPtr = oldDealer;	// reset dealer
+				attemptNoChange++;
+			}
+			attemptCount++;
+		}
+#if PEN_DEBUG
+		LOG.LogWarning(std::to_string(pushIn.fitness));
+#endif
+	} while (attemptNoChange != ATTEMPT_LIMIT);
+
+#if PEN_DEBUG
+	LOG.LogError("final Fitness: " + std::to_string(pushIn.fitness));
+	LOG.LogWarning("Highest Fitness: " + std::to_string(bestFitness));
+
+	for (auto& p : pushIn.push)
+	{
+		switch (p.aTable.gameName)
+		{
+		case Table::BJ:
+			LOG.LogWarning(std::to_string(p.aTable.number) + "     BJ     " + p.aDealerPtr->name);
+			break;
+		case Table::Rou:
+			LOG.LogWarning(std::to_string(p.aTable.number) + "     Rou    " + p.aDealerPtr->name);
+			break;
+		case Table::MB:
+			LOG.LogWarning(std::to_string(p.aTable.number) + "     MB     " + p.aDealerPtr->name);
+			break;
+		case Table::Poker:
+			LOG.LogWarning(std::to_string(p.aTable.number) + "     Poker  " + p.aDealerPtr->name);
+			break;
+		default:
+			LOG.LogError("Error: Invalid message in final Log");
+		}
+	}
+#endif
 }
