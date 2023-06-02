595b59c69f3033bb6f02d002a6c7e38dd73b4eb1
Thomas-Tran <thomas.tran0789@gmail.com>
2023-06-02 14:34:29 -0500

    Beginning to implement InstrumentationTimer project.

---
+++ PencilProgram/InstrumentorTimer/src/Timer.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
+#include <thread>
+
+struct Timer
+{
+	std::chrono::time_point<std::chrono::steady_clock> start, end;
+	std::chrono::duration<float> duration;
+
+	Timer();
+	~Timer();
+
+	void endTimer();
+};
+
+struct ProfileResult
+{
+	std::string Name;
+	long long Start, End;
+	uint32_t ThreadID;
+};
+
+struct InstrumentationSession
+{
+	std::string Name;
+};
+
+class Instrumentor
+{
+private:
+	InstrumentationSession* m_CurrentSession;
+	std::ofstream m_OutputStream;
+	int m_ProfileCount;
+
+public:
+	Instrumentor();
+	void BeginSession(const std::string& name, const std::string& filepath);
+	void EndSession();
+	void WriteProfile(const ProfileResult& result);
+	void WriteHeader();
+	void WriteFooter();
+	static Instrumentor& Get();
+};
+
+class InstrumentationTimer
+{
+private:
+	const char* m_Name;
+	std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
+	bool m_Stopped;
+
+public:
+	InstrumentationTimer(const char* name);
+	~InstrumentationTimer();
+	void Stop();
+};
